3a54f80c3fe0d3cb76034c88a8646b3b5dee8861
EpicFeed <d.melia@outlook.fr>
2023-11-27 07:50:44 +0100

    Initial commit - project generated using Spring Initialzr

---
+++ src/main/java/com/interactiveresume/Interactive/Resume/Backend/InteractiveResumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.interactiveresume.Interactive.Resume.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveResumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveResumeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interactiveresume/Interactive/Resume/Backend/InteractiveResumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interactiveresume.Interactive.Resume.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveResumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
